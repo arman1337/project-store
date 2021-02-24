979067b964b37c6bf50741387fe30058d394e075
arman1337 <amuratbekov30@gmail.com>
2021-02-24 13:32:39 +0600

    lab4

---
+++ src/app/products.ts
@@ -0,0 +1,112 @@
+export const products = [
+  {
+    id: 1,
+    name: 'HP 24mh FHD Monitor',
+    price: 164.99,
+    description: 'This FHD display with IPS technology gives you brilliant visuals and unforgettable quality; with a maximum resolution of 1920 x 1080 @ 75 Hz, you’ll experience the image accuracy and wide-viewing spectrums of premium tablets and mobile devices',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/91fAU6mxFsL._AC_SX679_.jpg',
+    rating: 4.7,
+    link: 'https://www.amazon.com/HP-24mh-FHD-Monitor-Built/dp/B08BF4CZSV/ref=sr_1_1?dchild=1&qid=1614100371&rnid=16225007011&s=computers-intl-ship&sr=1-1'
+  },
+  {
+    id: 2,
+    name: 'Acer SB220Q bi',
+    price: 93.99,
+    description: '21.5 inches Full HD (1920 x 1080) widescreen IPS display\n' +
+      'And Radeon free sync technology. No compatibility for VESA Mount',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/81QpkIctqPL._AC_SX679_.jpg',
+    rating: 4.7,
+    link: 'https://www.amazon.com/Acer-SB220Q-Ultra-Thin-Frame-Monitor/dp/B07CVL2D2S/ref=sr_1_2?dchild=1&qid=1614100371&rnid=16225007011&s=computers-intl-ship&sr=1-2'
+  },
+  {
+    id: 3,
+    name: 'HP VH240a',
+    price: 163,
+    description: 'RESOLUTION & PANEL — 23.8-inch Full HD monitor (1920 x 1080p @ 60 Hz) with 16:9 aspect ratio and an anti-glare matte IPS LED-backlit panel (2 million pixels, 16.7 million colors)',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/71trhuzbhML._AC_SY741_.jpg',
+    rating: 4.7,
+    link: 'https://www.amazon.com/HP-23-8-inch-Adjustment-Speakers-VH240a/dp/B072M34RQC/ref=sr_1_3?dchild=1&qid=1614100371&rnid=16225007011&s=computers-intl-ship&sr=1-3'
+  },
+  {
+    id: 4,
+    name: 'Sceptre 20',
+    price: 79.97,
+    description: '20" Led HD+ monitor high resolution 1600 x 900\n' +
+      '2 x HDMI ports ( convertible to DVI )\n' +
+      'Fast response time 5ms. Dimensions - With Stand (Inches)- 18.01 x 7.02 x13.57 .Without Stand (inches)- 18.01 x 1.32 x 10.43',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/617S7xFO58L._AC_SX679_.jpg',
+    rating: 4.5,
+    link: 'https://www.amazon.com/Sceptre-E205W-16003R-Frameless-Speakers-Metallic/dp/B07743412C/ref=sr_1_4?dchild=1&qid=1614100371&rnid=16225007011&s=computers-intl-ship&sr=1-4'
+  },
+  {
+    id: 5,
+    name: 'Sceptre Curved',
+    price: 166.99,
+    description: '27" Curved monitor up to 75Hz\n' +
+      'Fast Response Time: Fast response times reduce ghosting & blurring while transitioning pixels, always keeping the enemy & terrain precisely in focus during chaotic moments.',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/914W%2BOtJQ-L._AC_SX679_.jpg',
+    rating: 4.7,
+    link: 'https://www.amazon.com/Sceptre-Monitor-Speakers-Edge-Less-C275W-1920RN/dp/B07MTRQ6B3/ref=sr_1_5?dchild=1&qid=1614100371&rnid=16225007011&s=computers-intl-ship&sr=1-5'
+  },
+  {
+    id: 6,
+    name: 'LG 27GL83A-B',
+    price: 379.99,
+    description: '27 inch QHD (2560 X 1440) IPS display\n' +
+      'IPS 1ms response time & 144Hz refresh rate\n' +
+      'NVIDIA G-SYNC Compatible\n' +
+      '3-Side virtually borderless design\n' +
+      'Tilt / Height / Pivot adjustable stand',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/91lS5yEDH-L._AC_SX679_.jpg',
+    rating: 4.8,
+    link: 'https://www.amazon.com/LG-27GL83A-B-Ultragear-Compatible-Monitor/dp/B07YGZL8XF/ref=sr_1_6?dchild=1&qid=1614100371&rnid=16225007011&s=computers-intl-ship&sr=1-6'
+  },
+  {
+    id: 7,
+    name: 'Dell',
+    price: 150,
+    description: 'Device Type - LED-backlit LCD monitor - 27", Aspect Ratio -16:9, Contrast Ratio - 1000:1\n' +
+      'Panel Type - IPS, Full HD (1080p) 1920 x 1080 at 60 Hz, Response Time - 8 ms (grey-to-grey normal); 5 ms (grey-to-grey fast)',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/81nSaeP3AvL._AC_SX679_.jpg',
+    rating: 4.7,
+    link: 'https://www.amazon.com/Dell-backlit-Monitor-SE2719H-1080p/dp/B07KW6HFD1/ref=sr_1_7?dchild=1&qid=1614100371&rnid=16225007011&s=computers-intl-ship&sr=1-7'
+  },
+  {
+    id: 8,
+    name: 'LG 24M47VQ',
+    price: 99.99,
+    description: '2 MS response time. If using a wired connection, plug the Ethernet cable from your modem/Router to the input on your chromebase\n' +
+      '1920 x 1080 Full HD Resolution. OS Compatibility-Windows 8',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/91jzIGu5N-L._AC_SL1500_.jpg',
+    rating: 4.7,
+    link: 'https://www.amazon.com/LG-24M47VQ-24-Inch-LED-lit-Monitor/dp/B00W95RR32/ref=sr_1_8?dchild=1&qid=1614100371&rnid=16225007011&s=computers-intl-ship&sr=1-8'
+  },
+  {
+    id: 9,
+    name: 'BenQ',
+    price: 119.99,
+    description: 'Patented Eye-Care for extended use: Proprietary brightness Intelligence Adaptive technology adjusts brightness for comfortable viewing, low blue light and zero-flicker technology prevent headaches and eye strain. This eye tech delivers optimized images that are easy on your eyes.',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/712ZOa8Ze2L._AC_SL1280_.jpg',
+    rating: 4.7,
+    link: 'https://www.amazon.com/BenQ-proprietary-borderless-Brightness-GW2480/dp/B072XCZSSW/ref=sr_1_9?dchild=1&qid=1614100371&rnid=16225007011&s=computers-intl-ship&sr=1-9'
+  },
+  {
+    id: 10,
+    name: 'AOC CQ27G2',
+    price: 259.99,
+    description: '27" AOC G2 series gaming monitor with QHD 2K 2560x1440 Resolution\n' +
+      '1500R Super curved monitor wrapping around you for an immersive gaming experience\n' +
+      'Brilliant colors producing over 120% area coverage for sRGB color gamut',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/81oORaahKZL._AC_SL1500_.jpg',
+    rating: 4.7,
+    link: 'https://www.amazon.com/AOC-CQ27G2-Frameless-Adjustable-Guarantee/dp/B0862YHJ56/ref=sr_1_10?dchild=1&qid=1614100371&rnid=16225007011&s=computers-intl-ship&sr=1-10'
+  },
+
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
